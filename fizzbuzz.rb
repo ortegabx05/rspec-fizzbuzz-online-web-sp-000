e58bfe06f464224c7eeda243c6d3a24968a40783
jessi ortega <ortegabx05@gmail.com>
2019-06-13 18:10:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
-  "Fizz"
+   "Fizz"
 end
   if int % 5 == 0
-  "Buzz"
+   "Buzz"
 end
   if int % 3 == 0 && int % 5 == 0
-  "Fizzbuzz"
+   "Fizzbuzz"
  end
 end
